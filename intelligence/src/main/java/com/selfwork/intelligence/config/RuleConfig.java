d1ca1c8908abb9ad384191fe4ff1eab4e54e7ded
83908472@qq.com <83908472@qq.com>
2018-05-03 02:11:06 +0800

    數據同步

--- intelligence/src/main/java/com/selfwork/intelligence/config/RuleConfig.java
+++ intelligence/src/main/java/com/selfwork/intelligence/config/RuleConfig.java
@@ -9,19 +9,19 @@ import java.util.ArrayList;
  * Created by zzc on 2018/5/3.
  */
 @Component
-public class RuleFileLoader {
+public class RuleConfig {
 
     private static ArrayList<File> fileList = new ArrayList<File>();
 
     static {
-        // 读取规则文件
+        // 加载文件
         String path = ClassLoader.getSystemResource("rule").getPath();
-        readFile(path);
+        loadFile(path);
 
-        // 解析规则文件
+        // 解析文件
     }
 
-    public static void readFile(String filepath) {
+    public static void loadFile(String filepath) {
         File file= new File(filepath);
         if(!file.isDirectory()){
             fileList.add(file);
@@ -30,7 +30,7 @@ public class RuleFileLoader {
             for(File fileIndex:files){
                 //如果这个文件是目录，则进行递归搜索
                 if(fileIndex.isDirectory()){
-                    readFile(fileIndex.getPath());
+                    loadFile(fileIndex.getPath());
                 }else {
                     //如果文件是普通文件，则将文件句柄放入集合
                     fileList.add(fileIndex);
