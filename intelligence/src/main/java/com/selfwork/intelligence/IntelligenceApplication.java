e2fe2b725a87b7b986abfdb0ceb951712d0262ed
junlin.chen <calvincjl@hotmail.com>
2018-06-29 02:31:26 +0800

    解决打包问题，打包方式改为war

--- intelligence/src/main/java/com/selfwork/intelligence/IntelligenceApplication.java
+++ intelligence/src/main/java/com/selfwork/intelligence/IntelligenceApplication.java
@@ -3,7 +3,9 @@ package com.selfwork.intelligence;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,7 +15,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @EnableScheduling
 @ServletComponentScan //用于filter扫描
 @MapperScan("com.selfwork.intelligence.mapper")//将项目中对应的mapper类的路径加进来就可以了
-public class IntelligenceApplication {
+public class IntelligenceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
@@ -27,4 +29,10 @@ public class IntelligenceApplication {
         taskScheduler.setThreadNamePrefix("data-sync-task");
         return taskScheduler;
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(IntelligenceApplication.class);
+    }
+
 }
